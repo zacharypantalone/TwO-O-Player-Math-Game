cbf623a7ccce078f0252940d1d9753d894d3ad69
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-10-26 22:04:53 -0400

    working on gameplay logic

--- gameplay_logic.rb
+++ gameplay_logic.rb
@@ -1,3 +1,34 @@
 class Gameplay 
-  
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @current_player = player1
+    # self.nextTurn
+    
+    while true
+      self.nextTurn
+    end
+    # input = gets.chomp
+    # if input == "1"
+    #   puts true
+    # else 
+    #   puts false
+    # end  
+  end
+
+  attr_reader :current_player
+
+  def nextTurn
+    input = gets.chomp
+    answer = "#{@current_player.name} said #{input}"
+    puts answer
+
+
+    if @player1.name == @current_player.name
+      @current_player = @player2
+    else 
+      @current_player = @player1
+    end
+  end 
 end
--- main.rb
+++ main.rb
@@ -4,5 +4,7 @@ require './gameplay_logic'
 
 player_1 = Player.new("Player 1")
 player_2 = Player.new("Player 2")
+# puts player_1.name
 
-puts player_1.name
+game = Gameplay.new(player_1, player_2)
+puts game.current_player.name
