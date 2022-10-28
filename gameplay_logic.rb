0ff586212d69e8ed1b1094e58e38b97f6c762672
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-10-27 20:06:15 -0400

    cleaning code

--- gameplay_logic.rb
+++ gameplay_logic.rb
@@ -6,58 +6,48 @@ class Gameplay
     @current_player = player1
     @question = Question.new
     @life = 3
-    
   end
+    
   
   def startGame
-    # we need to find something that we can trigger to break the loop
+    
     while @player1.life > 0 && @player2.life > 0
       nextTurn
       @question = Question.new
-      
     end
     puts "Game over, thanks for playing!"
+
   end
-  
-  
-
- 
-
-   
-  
+      
   attr_reader :current_player
-
-
+  
   def correctAnswer
-    
     @input = gets.chomp.to_i
-    
     prompt = "#{@current_player.name} said #{@input}"
     puts prompt
+    
     if !@input.is_a? Integer
         puts "Invalid input!"
     end
 
     if @input == @question.answer
         puts "#{@question.answer} is correct!"
-      else
-        puts "Yikes! That's incorrect!"
-        @current_player.life -= 1
+    else
+      puts "Yikes! That's incorrect!"
+      @current_player.life -= 1
     end
-    
-  end
 
+  end
+    
   attr_reader :input
 
   def nextTurn
     puts @question.question_prompt
     correctAnswer
     if @current_player.name == @player1.name
-      @current_player = @player2
+       @current_player = @player2
     else
       @current_player = @player1
     end
-   
-    
-  end 
-end 
+  end
+end
